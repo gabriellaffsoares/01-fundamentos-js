bfa2ed652abc0242c093fd9e959cbfab625d25fd
André Luis Soares Alves <andrekd3@gmail.com>
2022-08-10 21:07:09 -0300

    exemplos da aula 1

---
+++ tipoString.js
@@ -0,0 +1,26 @@
+//Tipo String
+//Guardar textos 
+
+const texto1 = "Olá Mundo!";
+const texto2 = 'Olá Mundo!';
+const senha = 'senharlegal1236@';
+const StringNumeros = '123123456';
+
+const citacao = 'O Leo disse "oi"';
+
+console.log(texto1)
+console.log(texto2)
+console.log(senha)
+console.log(StringNumeros)
+console.log(citacao)
+
+//Concatenação (+)
+
+const nome = 'Gabriella'
+const sobrenome = 'Soares'
+
+const nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nomeCompleto)
+
+//template string ou template literal
